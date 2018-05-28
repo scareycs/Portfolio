798fd1975d71c13cb5e10661eba64ec6a654bdea
scareycs <scareycs@gmail.com>
2018-05-27 23:22:26 -0400

    added anchor to portfolio sections

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function scrollToAnchor(section){
+    var aTag = $("a[name='"+ section +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'medium');
+}
+
+$("#home").click(function(){
+	scrollToAnchor('home');
+});
+
+$("#projects").click(function(){
+	scrollToAnchor('projects');
+});
+
+$("#about").click(function(){
+	scrollToAnchor('about');
+});
+
+$("#contact").click(function(){
+	scrollToAnchor('contact');
+});
